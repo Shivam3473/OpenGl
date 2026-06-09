ad1ff54c8244ef72f49c96380877eeccf2daf63b
SHIVAM TIBREWAL <tibrewalshivam05@gmail.com>
2026-06-09 12:43:55 +0530

    tut2

--- MyProject/src/tut2.cpp
+++ MyProject/src/tut2.cpp
@@ -1,28 +1,33 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+// here we will learn about making a triable in openGl and we have got the code for the screen with the boilerplate named screen
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
-void size_callback(GLFWwindow* window, int width, int height){
+void size_callback(GLFWwindow *window, int width, int height)
+{
     glViewport(0, 0, width, height);
 }
 
-int main(){
+int main()
+{
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "WindowTitle", NULL, NULL);
-    if(window == NULL){
+    GLFWwindow *window = glfwCreateWindow(800, 600, "WindowTitle", NULL, NULL);
+    if (window == NULL)
+    {
         cout << "Failed to create window" << endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         cout << "Failed to initialize GLAD" << endl;
         return -1;
     }
@@ -30,11 +35,29 @@ int main(){
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, size_callback);
 
-    while(!glfwWindowShouldClose(window)){
+    while (!glfwWindowShouldClose(window))
+    {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // all the above code foused on producing much of the screen now we will learn separately extra code needed to make a triangle
+
+    // since it contains 3 vertices thus we need 3 set of arrays of 3d coordinates
+
+    // OpenGL only processes 3D coordinates when they're in a specific range between -1.0 and 1.0 on all 3 axes (x, y and z). All coordinates within this so called normalized device coordinates range will end up visible on your screen (and all coordinates outside this region won't).
+
+    // Because we want to render a single triangle we want to specify a total of three vertices with each vertex having a 3D position. We define them in normalized device coordinates (the visible region of OpenGL) in a float array
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    // here these will be passed to the vertex shaders and then will be returned as screen space coordinates and then transformed to fragments as inputs to ur fragment shader
+
+
+    // also we need to use vertex buffer object to feed this data of vertex to the graphic card for the intant acess to the vertex shaders
     glfwTerminate();
     return 0;
 }
